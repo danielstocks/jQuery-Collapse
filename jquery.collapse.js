8d35dde155d974e30d7aa5a65c10b76e23c1df3d
Daniel Stocks <daniel@webcloud.se>
2010-06-30 11:46:47 +0200

    delegating click events

--- jquery.collapse.js
+++ jquery.collapse.js
@@ -2,12 +2,14 @@
  * Collapse plugin for jQuery
  * ---
  * @author Daniel Stocks (http://webcloud.se)
- * @version 0.1
- * @updated 28-JUN-2010
+ * @version 0.2
+ * @updated 30-JUN-2010
  * ---
  * Note: For cookie support you need to include
  * the cookie plugin from here: http://plugins.jquery.com/project/cookie
  * ---
+ * Project Page & Documentation:
+ * http://github.com/danielstocks/jQuery-Collapse/
  */
  
 (function($) {
@@ -19,7 +21,7 @@
                 inactive : "inactive",
                 active : "active",
                 head : "h3",
-                group : "ul",
+                group : "ul, div",
                 speed : 100,
                 cookieID : "collapse",
                 disableCookie : false
@@ -28,26 +30,31 @@
             var op = $.extend(defaults, options),
                 cookie_counter = 0;
                 cookieEnable = false;
-                
+            
+            // Is cookie support available?    
             if($.cookie) {
                 var cookieEnable = true;
             }
+            // Or disabled by user?
             if(op.disableCookie) {
                 cookieEnable = false;
             }
             
+            // Reference to the object
+            var obj = $(this);
+            
             // Event for showing
-            $(this).bind("show", function(e, el) {
+            obj.bind("show", function(e, el) {
                 $.each(el, function() {
                     $(this).show().prev().removeClass(op.inactive).addClass(op.active);
                 });
             });
             
             // event method for hiding
-            $(this).bind("hide", function(e, el) {
+            obj.bind("hide", function(e, el) {
                 $.each(el, function() {
                     $(this).hide().prev().removeClass(op.active).addClass(op.inactive);
-                }); 
+                });
             });
                 
             return this.each(function() {
@@ -77,29 +84,36 @@
                         }
                     }
                 }
-                sections.bind("click", function(e) {
+                // Delegate click event to show/hide content
+                obj.bind("click", function(e) {
                     e.preventDefault();
+                    var t = $(e.target);
+                    if(!t.is(op.head)) {
+                        return false;
+                    }
                     var num = sections.index(this),
                         cookieName = cookie + num,
                         cookieVal = num,
-                        ul = $(this).next(op.group);
+                        content = t.next(op.group);
                     // Hide
-                    if($(this).hasClass(op.active)) {
-                        ul.slideUp(op.speed, function() {
-                            $(this).trigger('hide', [$(this)]);
+                    if(t.hasClass(op.active)) {
+                        content.slideUp(op.speed, function() {
+                            content.trigger('hide', [content]);
                         })
                         cookieVal += 'closed';
-                        if(cookieEnable)
+                        if(cookieEnable) {
                             $.cookie(cookieName, cookieVal, { path: '/', expires: 10 });
+                        }
                         return;
                     }
                     // Show
-                    ul.slideDown(op.speed, function() {
-                        $(this).trigger('show', [$(this)]);
+                    content.slideDown(op.speed, function() {
+                        content.trigger('show', [content]);
                     });
                     cookieVal += 'open';
-                    if(cookieEnable)
+                    if(cookieEnable) {
                         $.cookie(cookieName, cookieVal, { path: '/', expires: 10 });
+                    }
                 });
             });
         }
