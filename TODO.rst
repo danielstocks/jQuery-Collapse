c2257737098a1da321469c5527375b91377d8081
Daniel Stocks <daniel@webcloud.se>
2010-10-18 16:39:53 +0200

    todos

--- TODO.rst
+++ TODO.rst
@@ -1,6 +1,9 @@
 ROADMAP
 -------
 
+Rewrite group and head logic to allow for a more flexible markup.
+
+
 v0.8.8
 ----
 * Write more thorough tests
--- src/jquery.collapse.js
+++ src/jquery.collapse.js
@@ -1,11 +1,8 @@
-/*!
+/*
  * Collapse plugin for jQuery
  * http://github.com/danielstocks/jQuery-Collapse/
  *
  * @author Daniel Stocks (http://webcloud.se)
- * @version 0.9.1
- * @updated 17-AUG-2010
- * 
  * Copyright 2010, Daniel Stocks
  * Released under the MIT, BSD, and GPL Licenses.
  */
@@ -138,14 +135,15 @@
     });
 
     // Make sure can we eat cookies without getting into trouble.
-    var cookieSupport = (function() {
+    var cookie = true;
+    $(function() {
         try {
             $.cookie('x', 'x', { path: '/', expires: 10 });
-            $.cookie('x', null);
         }
         catch(e) {
-            return false;
+            cookie = false;
+            $.cookie('x', null);
         }
-        return true;
-    })();
+    });
+    var cookieSupport = $.fn.collapse.cookieSupport = cookie;
 })(jQuery);
